f220e37a74dcd1fecfd748e13fd9b38d138c6647
mateonav98 <mateonav98@gmail.com>
2022-05-17 21:14:12 -0700

    getting specific weather data in console log

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,8 @@
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 // WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// THEN I am presented with the city name, the date, a
+// n icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 // WHEN I view future weather conditions for that city
@@ -13,7 +14,7 @@
 // let citySearch = document.querySelector("#citySearch")
 
 apiKey = "c46f2990592153f3323f237a20612ab6"
-
+city = "San Jose, California"
 function weatherData (city) {
   let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
   fetch(queryUrl)
@@ -22,8 +23,13 @@ function weatherData (city) {
       return response.json();
     }})
     .then(function(data){
-      console.log(data)
+      console.log(data);
+      console.log (data.name)
+      console.log(data.weather.icon)
+      console.log(data.main.temp)
+      console.log(data.main.humidity)
+      console.log(data.wind.speed)
     })
   }
 
-  weatherData ("San Jose, California")
+  weatherData (city)
